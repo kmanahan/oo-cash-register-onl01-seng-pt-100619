dc679c04a9c7742bff4ace4ba65af34eb6431e7f
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-15 20:34:15 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require "pry" 
 
+class CashRegister 
+attr_accessor :total, :discount, :items
+  def initialize(discount=0)  
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  
+  def add_item(title, price, quantity = 1) 
+    self.total += price * quantity
+    if quantity > 1 
+    @items << title
+   end 
+  end 
+  #binding.pry
+  def apply_discount
+    #binding.pry
+    self.total -= self.total * (discount.to_f/100)
+    puts "After the discount, the total comes to #{total}."
+  end 
+
+end 
