e078da4892d3c0d76c900b2fe34c80ecee09c71a
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-15 23:07:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,11 +20,17 @@ attr_accessor :total, :discount, :items
   def apply_discount
     #binding.pry
     if discount > 0 
+<<<<<<< HEAD
       self.total -= self.total * (discount.to_f/100)
       "After the discount, the total comes to $#{total.to_i}."
     else 
       "There is no discount to apply."
     end
+=======
+    self.total -= self.total * (discount.to_f/100)
+    "After the discount, the total comes to $#{total.to_i}."
+  else 
+>>>>>>> e1c1ac77a27216b550cfc98a8026643bc3441b6b
   end 
   
   
